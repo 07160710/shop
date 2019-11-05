0deaba9b654a6ab0a73ab09e9f1f612b15ef8974
huangkaifeng <1195745286@qq.com>
2019-11-05 10:34:33 +0800

    登录功能

---
+++ SRP/check_login.php
@@ -0,0 +1,16 @@
+<?php
+require_once("../../public/public_param.php");
+if($_SESSION['u_id']==""){
+    header("Location:../../manage_login.php?logout=0");
+    exit;
+}
+else{
+    if(isset($_SESSION['session_time'])){
+        $elapsed_time = time()-$_SESSION['session_time'];
+        if($elapsed_time >= SESSION_TIMEOUT){
+            header("Location:../../manage_login.php?logout=2");
+            exit;
+        }
+    }
+    $_SESSION['session_time'] = time();
+}
